b0a75be3a20c0c7c11cc1606bbbaaca6577122d7
RafalJackiewicz <jackiewicz.rafall@gmail.com>
2021-06-09 23:16:50 +0200

    Update

--- script/script.js
+++ script/script.js
@@ -1,88 +1,79 @@
 "use strict";
-let playerOne=[0,0,0,0,0];
-let playerTwo=[0,0,0,0,0];
-let players=[playerOne,playerTwo];
-let currentRound=0;
-let playWithComputer=false;
+let playerOne = [0, 0, 0, 0, 0];
+let playerTwo = [0, 0, 0, 0, 0];
+let players = [playerOne, playerTwo];
+let currentRound = 0;
+let playWithComputer = false;
+const colorNewRound = '#d99d3d';
 
 // pictures in variables
-const diceNull='dice-null.png';
-const diceOne='dice-one.png'
-const diceTwo='dice-two.png'
-const diceThree='dice-three.png'
-const diceFour='dice-four.png'
-const diceFive='dice-five.png'
-const diceSix='dice-six.png'
-const allDices=[diceOne,diceTwo,diceThree,diceFour,diceFive,diceSix];
+const diceNull = 'dice-null.png';
+const diceOne = 'dice-one.png'
+const diceTwo = 'dice-two.png'
+const diceThree = 'dice-three.png'
+const diceFour = 'dice-four.png'
+const diceFive = 'dice-five.png'
+const diceSix = 'dice-six.png'
+const allDices = [diceOne, diceTwo, diceThree, diceFour, diceFive, diceSix];
 
 //which dice want players change?
-let changeTheRollPlayerOne=[0,0,0,0,0];
-let changeTheRollPlayerTwo=[0,0,0,0,0];
-let kindOfRolling={
-    "Poker":7,
-    "Four of a kind":6,
-    "Full house":5,
-    "Straight":4,
-    "Three of a kind":3,
-    "Two pair":2,
-    "One pair":1,
-    "High card":0
+let changeTheRollPlayerOne = [0, 0, 0, 0, 0];
+let changeTheRollPlayerTwo = [0, 0, 0, 0, 0];
+let kindOfRolling = {
+  "Poker": 7,
+  "Four of a kind": 6,
+  "Full house": 5,
+  "Straight": 4,
+  "Three of a kind": 3,
+  "Two pair": 2,
+  "One pair": 1,
+  "High card": 0
 }
 
 //declare a variables from html
-let dices=document.querySelectorAll(".dice");
-const buttonRoll=document.querySelector(".buttonRoll>input");
-const headPlayerTwoFirst=document.querySelector(".scoreBoardTwo>h2");
-const headPlayerTwoSecond=document.querySelector(".nameOfPlayerTwo>h2");
-const checkBox=document.querySelector("input[type=checkbox]");
-const countWinsPlayerOne=document.querySelector('.countWinsPlayerOne');
-const countWinsPlayerTwo=document.querySelector('.countWinsPlayerTwo');
-const resultOfPlayerOne=document.querySelector('.resultOfPlayerOne>h2');
-const resultOfPlayerTwo=document.querySelector('resultOfPlayerTwo>h2');
-const firstRoundPOne=document.querySelector('.firstRoundPOne>h3');
-const firstRoundPTwo=document.querySelector('.firstRoundPTwo>h3');
-const secondRoundPOne=document.querySelector('.secondRoundPOne>h3');
-const secondRoundPTwo=document.querySelector('.secondRoundPTwo>h3');
+let dices = document.querySelectorAll(".dice");
+const buttonRoll = document.querySelector(".buttonRoll>input");
+const headPlayerTwoFirst = document.querySelector(".scoreBoardTwo>h2");
+const headPlayerTwoSecond = document.querySelector(".nameOfPlayerTwo>h2");
+const checkBox = document.querySelector("input[type=checkbox]");
+const countWinsPlayerOne = document.querySelector('.countWinsPlayerOne');
+const countWinsPlayerTwo = document.querySelector('.countWinsPlayerTwo');
+const resultOfPlayerOne = document.querySelector('.resultOfPlayerOne>h2');
+const resultOfPlayerTwo = document.querySelector('resultOfPlayerTwo>h2');
+const firstRoundPOne = document.querySelector('.firstRoundPOne>h3');
+const firstRoundPTwo = document.querySelector('.firstRoundPTwo>h3');
+const secondRoundPOne = document.querySelector('.secondRoundPOne>h3');
+const secondRoundPTwo = document.querySelector('.secondRoundPTwo>h3');
 
-checkBox.addEventListener("change",()=>{
-  if (checkBox.checked){
-    playWithComputer=false;
-    headPlayerTwoFirst.innerHTML="Player 2";
-    headPlayerTwoSecond.innerHTML="Player 2";
-  } else {
-    playWithComputer=true;
-    headPlayerTwoFirst.innerHTML="Computer";
-    headPlayerTwoSecond.innerHTML="Computer";
-  }
-});
+
 
 //Function show correct pictures of Dice on browser
 //dodać animację jak losują się kości
-function changePictureOfDice(){
-  let i=0;
-  for (const dice of dices){
-    let number=players[Math.floor(i/5)][i%5];
-    switch (number){
+function changePictureOfDice() {
+  let i = 0;
+  for (const dice of dices) {
+    let number = players[Math.floor(i / 5)][i % 5];
+    switch (number) {
       case 0:
-        dice.setAttribute("src",`img/${diceNull}`);
+        dice.setAttribute("src", `img/${diceNull}`);
         break;
       case 1:
-        dice.setAttribute("src",`img/${diceOne}`);
+        dice.setAttribute("src", `img/${diceOne}`);
         break;
       case 2:
-        dice.setAttribute("src",`img/${diceTwo}`);
+        dice.setAttribute("src", `img/${diceTwo}`);
         break;
       case 3:
-        dice.setAttribute("src",`img/${diceThree}`);
+        dice.setAttribute("src", `img/${diceThree}`);
         break;
       case 4:
-        dice.setAttribute("src",`img/${diceFour}`);
+        dice.setAttribute("src", `img/${diceFour}`);
         break;
       case 5:
-        dice.setAttribute("src",`img/${diceFive}`);
+        dice.setAttribute("src", `img/${diceFive}`);
         break;
       case 6:
-        dice.setAttribute("src",`img/${diceSix}`);
+        dice.setAttribute("src", `img/${diceSix}`);
         break;
     }
     i++;
@@ -90,12 +81,50 @@ function changePictureOfDice(){
 }
 
 //Event Listeners
-buttonRoll.addEventListener("click", ()=>{
+checkBox.addEventListener("change", () => {
+  if (checkBox.checked) {
+    playWithComputer = false;
+    headPlayerTwoFirst.innerHTML = "Player 2";
+    headPlayerTwoSecond.innerHTML = "Player 2";
+  } else {
+    playWithComputer = true;
+    headPlayerTwoFirst.innerHTML = "Computer";
+    headPlayerTwoSecond.innerHTML = "Computer";
+  }
+});
+
+buttonRoll.addEventListener("click", () => {
   currentRound++;
 })
-buttonRoll.addEventListener("click",()=>{
-  switch (currentRound){
+buttonRoll.addEventListener("click", () => {
+  switch (currentRound) {
     case 1:
+      firstRoundPOne.style.color = `${colorNewRound}`;
+      break;
+    case 2:
+      firstRoundPTwo.style.color = `${colorNewRound}`;
+      break;
+    case 3:
+      console.log('wybór kości do zmiany gracza pierwszego');
+      break;
+    case 4:
+      console.log('wybór kości do zmiany gracza drugiego');
+      break;
+    case 5:
+      secondRoundPOne.style.color = `${colorNewRound}`;
+      break;
+    case 6:
+      secondRoundPTwo.style.color = `${colorNewRound}`;
+      break;
+    case 7:
+      firstRoundPOne.style.color = 'initial';
+      firstRoundPTwo.style.color = 'initial';
+      secondRoundPOne.style.color = 'initial';
+      secondRoundPTwo.style.color = 'initial';
+      currentRound = 0;
+      break;
+
+
 
   }
 })
